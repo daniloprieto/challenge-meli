70835570f484451f71950c00f4165fb917f2cee0
Danilo Prieto <danilo.s.prieto@gmail.com>
2022-05-21 13:24:56 -0300

    Design pdp

--- client/src/components/breadcrumb/breadcrumb.tsx
+++ client/src/components/breadcrumb/breadcrumb.tsx
@@ -5,11 +5,11 @@ export const Breadcrumb = () => {
   return (
     <div className="breadcrumb__div">
       <ul className="breadcrumb__ul">
-        <a href="#">Volver al listado {'>'} </a>
-        <a href="#">Computación {'>'} </a>
-        <a href="#">Accesorios para PC {'>'} </a>
-        <a href="#">Auriculares {'>'} </a>
-        <a href="#">Teclados {'>'} </a>
+        <a href="#">Volver al listado</a>
+        <a href="#">Computación</a>
+        <a href="#">Accesorios para PC</a>
+        <a href="#">Auriculares</a>
+        <a href="#">Teclados</a>
       </ul>
     </div>
   )
--- client/src/components/product/product.tsx
+++ client/src/components/product/product.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import './product.scss';
+import product from "../../assets/images/product.jpg";
 
 export const Product = () => {
   return (
-    <div>product</div>
+    <div className='product'>
+      <div className='panel'>
+        <div className='panel__column'>
+          <div className='panel__gallery'>
+            <img src={ product } alt="product detail" />
+          </div>
+          <Data />
+        </div>
+        <div className='panel__column'>
+          <div className='panel__gallery'>
+            <img src="" alt="" />
+          </div>
+          <Seller />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Data = () => {
+  return (
+    <div className='data'>
+      <h4>Descripción</h4>
+      <p>Sony, sin lugar a dudas es una de las marcas más reconocidas en el mundo por la fabricación de dispositivos de audio. Su gama de auriculares se caracteriza por brindar siempre una gran experencia de uso en sus usuarios y ofrecer una alta calidad en todos los componentes de sus reproductores. Esto hace que puedas notar un gran sonido desde su primer uso.</p>
+    </div>
+  )
+}
+
+const Seller = () => {
+  return (
+    <div className='seller'>
+      <span>Nuevo {'>'} 32 vendidos</span>
+      <h4>Auriculares inalámbricos Sony WH-CH510 negro</h4>
+      <p>$1980</p>
+      <button className='btn-accept'>Comprar</button>
+    </div>
   )
 }
